f40f0a5d4cd2562a7db99bc1bc055ed4e7efd957
techTomita <tech.tomita@gmail.com>
2022-06-10 05:17:55 +0000

    fix mail to

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
     def contact_mail(contact)
         @contact = contact
         
-        mail to: ENV["MAILTO"], subject: "お問合せ内容"
+        mail to: ENV["TOMAIL"], subject: "お問合せ内容"
     end
 end
